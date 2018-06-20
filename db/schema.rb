e1b86bfb37226b5348bed3b0501b1c943c4f19fa
Alexander George Legaspi <lex83nyc@gmail.com>
2018-06-20 16:47:58 -0400

    fixed schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_06_20_193519) do
 
   # These are extensions that must be enabled in order to support this database
@@ -35,6 +34,17 @@ ActiveRecord::Schema.define(version: 2018_06_20_193519) do
     t.index ["students_id"], name: "index_events_students_on_students_id"
   end
 
+  create_table "guests", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "company"
+    t.string "company_website"
+    t.string "email"
+    t.string "linkedin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -44,7 +54,6 @@ ActiveRecord::Schema.define(version: 2018_06_20_193519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   add_foreign_key "events_students", "events", column: "events_id"
   add_foreign_key "events_students", "students", column: "students_id"
