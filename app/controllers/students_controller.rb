9d9ce29de89ba29d72cdd63115c8f7c2532a9501
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-06-20 11:24:41 -0400

    fixed routes and views added

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,32 @@
+class StudentsController < ApplicationController
+	def index
+		@students = Student.all
+		
+	end
+
+	def create
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def show
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+
+end
