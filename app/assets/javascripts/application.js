abb0640875134e6c14025099bf5564168f1a8ff2
Alexander George Legaspi <lex83nyc@gmail.com>
2018-06-21 14:04:41 -0400

    updated application layout. added rough logo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,13 @@
 //= require toastr/build/toastr.min.js
 //= require rails-ujs
 //= require_tree .
+
+// Initialize tooltip component
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+  
+  // Initialize popover component
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
