f8a647483a76f77dd03e42b1f6370ba3854e16a8
Alex G. Legaspi <38141232+aglegaspi@users.noreply.github.com>
2018-06-22 13:06:37 -0400

    cleaned up error

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-			root :to => 'home#index'
+	root :to => 'home#index'
 
-      resources :event_guests
-			resources :guests 
-			end
-			resources :events
-			resources :students do
-				resources :events_students, only: [:new, :create]
-			end
+	resources :event_guests
+	resources :events
+	
+	resources :students do
+		resources :events_students, only: [:new, :create]
+	end
 end
