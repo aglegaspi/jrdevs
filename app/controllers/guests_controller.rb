a61efd2d9a61b1adfde17ce26f67b09f33a7b382
Alex G. Legaspi <38141232+aglegaspi@users.noreply.github.com>
2018-06-22 12:56:31 -0400

    Update guests_controller.rb

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -10,10 +10,9 @@ class GuestsController < ApplicationController
       @eventguests = EventGuest.all
   end
 
-  def create
-    		@guest = Guest.create(guest_params)
- 
-        eventguest = EventGuest.create(event_id: params[:event_id],guest_id: @guest.id )
+def create
+  @guest = Guest.create(guest_params)
+  eventguest = EventGuest.create(event_id: params[:event_id],guest_id: @guest.id )
         if @guest.valid?
               flash[:success] = 'Your guest has been successfully created'
               redirect_to guests_path
@@ -24,35 +23,28 @@ class GuestsController < ApplicationController
   end
 
   def show
-
       @guest_show = Guest.find(params[:id])
-
   end
+  
   def edit
-
       @guest = Guest.find(params[:id])
-
   end
 
-  def update
-           guest = Guest.find(params[:id])
-           guest.update(guest_params)
-           redirect_to guests_path
-
-      end
+def update
+  guest = Guest.find(params[:id])
+  guest.update(guest_params)
+  redirect_to guests_path
+end
 
 
   private
-
-    def event_guests_params
+  def event_guests_params
         params.require(:event_guest).permit(:event_id, :guest_id)
     end
 
     def guest_params
-  		params.require(:guest).permit(:first_name, :last_name, :company,
-                                    :company_website, :email, :linkedin)
+  		params.require(:guest).permit(:first_name, :last_name, :company, :company_website, :email, :linkedin)
   	end
 
-
-
+  
 end
