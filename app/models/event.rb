80b422ce34ebd94e10905eee9a5c5df35ff65e90
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-06-26 02:01:28 -0400

    pics fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
   has_many :event_guests, dependent: :destroy
   has_many :guests, through: :event_guests
 
+  has_many :events_students, dependent: :destroy
+  has_many :students, through: :events_students
+  
+
+
 end
