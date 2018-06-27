1f2dedb74bf91de0e461ae112ba8a8910de6cad1
bdb <bhuiya@github.com>
2018-06-27 14:50:33 -0400

    guest layout changed

--- app/assets/javascripts/guests.js
+++ app/assets/javascripts/guests.js
@@ -1,8 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+
+
 console.log(' Document Loading');
 $(document).ready(function() {
+
+  $(1).fadeIn();
+  $(2).fadeIn("slow");
+  $(3).fadeIn(3000);
+
   if (document.querySelector('body.guests.index') == undefined) {
     console.log('body.guests.index');
     return;
